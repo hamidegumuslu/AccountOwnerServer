8f1903d4fc1f06ad7a2f617e02e074eb22c23c2d
Hamide Gümüşlü <hamidegumuslu@gmail.com>
2019-12-30 14:37:12 +0300

    Added entity framework core and ef core mysql

---
+++ Entities/RepositoryContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Entities
+{
+    public class RepositoryContext: DbContext
+    {
+        public RepositoryContext(DbContextOptions options)
+            :base(options)
+        {
+        }
+
+        public DbSet<Owner>  Owners { get; set; }
+        public DbSet<Account> Acoounts { get; set; }
+    }
+}
